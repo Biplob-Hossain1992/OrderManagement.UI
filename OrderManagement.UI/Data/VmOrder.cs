3a8908c480ebf37262eb98f4b7dbf43009ae7e59
Biplob-Hossain1992 <biplobcse.diu@gmail.com>
2024-07-16 00:58:06 +0600

    Add project files.

---
+++ OrderManagement.UI/Data/VmOrder.cs
@@ -0,0 +1,16 @@
+﻿namespace OrderManagement.UI.Data
+{
+    #nullable disable
+    public class VmOrder
+    {
+        public int ReferenceId { get; set; }
+        public string OrderNo { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string SupplierName { get; set; } = "";
+        public DateTime ExpectedDate { get; set; }
+        public string Remarks { get; set; }
+        public int SupplierId { get; set; }
+        public int ItemId { get; set; }
+        public List<VmTableValuedParameter> OrderItem { get; set; } = new List<VmTableValuedParameter>();
+    }
+}
---
+++ OrderManagement.UI/Data/VmTableValuedParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagement.UI.Data
+{
+    public class VmTableValuedParameter
+    {
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
